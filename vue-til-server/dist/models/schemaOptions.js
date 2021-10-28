45b31988bf28b8fba86bd5d2f644b2fc1ef48bf2
Walter <jh3010@gmail.com>
2021-10-28 21:23:01 +0900

    커밋

--- vue-til-server/dist/models/schemaOptions.js
+++ vue-til-server/dist/models/schemaOptions.js
@@ -1,13 +1,6 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-var _default = {
+export default {
   timestamps: {
     createdAt: 'created_at'
   }
 };
-exports.default = _default;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9tb2RlbHMvc2NoZW1hT3B0aW9ucy5qcyJdLCJuYW1lcyI6WyJ0aW1lc3RhbXBzIiwiY3JlYXRlZEF0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7ZUFBZTtBQUNiQSxFQUFBQSxVQUFVLEVBQUU7QUFDVkMsSUFBQUEsU0FBUyxFQUFFO0FBREQ7QUFEQyxDIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQge1xyXG4gIHRpbWVzdGFtcHM6IHtcclxuICAgIGNyZWF0ZWRBdDogJ2NyZWF0ZWRfYXQnLFxyXG4gIH0sXHJcbn07XHJcbiJdfQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9tb2RlbHMvc2NoZW1hT3B0aW9ucy5qcyJdLCJuYW1lcyI6WyJ0aW1lc3RhbXBzIiwiY3JlYXRlZEF0Il0sIm1hcHBpbmdzIjoiQUFBQSxlQUFlO0FBQ2JBLEVBQUFBLFVBQVUsRUFBRTtBQUNWQyxJQUFBQSxTQUFTLEVBQUU7QUFERDtBQURDLENBQWYiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCB7XHJcbiAgdGltZXN0YW1wczoge1xyXG4gICAgY3JlYXRlZEF0OiAnY3JlYXRlZF9hdCcsXHJcbiAgfSxcclxufTtcclxuIl19
