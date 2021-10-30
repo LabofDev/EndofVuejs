be3f7dc444d8301352334209a19c74aaece3a6f6
Walter <jh3010@gmail.com>
2021-10-30 23:17:45 +0900

    code spliting 적용

--- vue-til/src/App.vue
+++ vue-til/src/App.vue
@@ -1,26 +1,15 @@
 <template>
-	<div id="app">
-		<img alt="Vue logo" src="./assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+	<div>
+		<header>
+			<router-link to="/login">로그인</router-link> |
+			<router-link to="/signup">회원가입</router-link>
+		</header>
+		<router-view></router-view>
 	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import AppHeader from './components/common/AppHeader.vue';
-
-export default {
-	name: 'App',
-	components: {
-		HelloWorld,
-		AppHeader,
-	},
-	created() {
-		var a = 10;
-		console.log(a);
-		console.log('Hi');
-	},
-};
+export default {};
 </script>
 
 <style>
