75475820b6a078ac4360ccfcedde95b2dfaaad88
Walter <jh3010@gmail.com>
2021-11-09 08:00:05 +0900

    목록 조회 초기 커밋

--- vue-til/src/api/common/interceptors.js
+++ vue-til/src/api/common/interceptors.js
@@ -5,7 +5,7 @@ export function setInterceptors(instance) {
 	instance.interceptors.request.use(
 		function (config) {
 			// Do something before request is sent
-			console.log(config);
+			// console.log(config);
 			config.headers.Authorization = store.state.token;
 			return config;
 		},
--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -2,12 +2,36 @@
 	<div>
 		<div class="main list-container contents">
 			<h1 class="page-header">Today I Learned</h1>
+			<ul>
+				<li v-for="postItem in postItems" v-bind:key="postItem._id">
+					{{ postItem.title }}
+				</li>
+			</ul>
 		</div>
 	</div>
 </template>
 
 <script>
-export default {};
+import { fetchPosts } from '@/api/index';
+
+export default {
+	data() {
+		return {
+			postItems: [],
+		};
+	},
+	methods: {
+		async fetchData() {
+			// const response = await fetchPosts();
+			// console.log(response);
+			const { data } = await fetchPosts();
+			this.postItems = data.posts;
+		},
+	},
+	created() {
+		this.fetchData();
+	},
+};
 </script>
 
 <style></style>
