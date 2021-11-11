872dd59bc8ea524a5579da13a8e71514987f54ba
Walter <jh3010@gmail.com>
2021-11-12 07:38:30 +0900

    validation 추가

--- vue-til/src/components/posts/PostAddForm.vue
+++ vue-til/src/components/posts/PostAddForm.vue
@@ -10,6 +10,9 @@
 				<div>
 					<label for="contents">Contents: </label>
 					<textarea id="contents" type="text" rows="5" v-model="contents" />
+					<p v-if="!isContentsValid" class="validation-text warning">
+						Contents length must be less than 200.
+					</p>
 				</div>
 				<button type="submit" class="btn">Create</button>
 			</form>
@@ -27,6 +30,11 @@ export default {
 			contents: '',
 			logMessage: '',
 		};
+	},
+	computed: {
+		isContentsValid() {
+			return this.contents.length <= 200;
+		},
 	},
 	methods: {
 		async submitForm() {
